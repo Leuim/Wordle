24d8b06a6b1fca8c2f5ba1dae49e0dad11c25f61
leuim <jassim.mohammed48@gmail.com>
2025-05-10 20:35:42 +0300

    start javascript. some varibles and chached elements done. printing on tiles done

--- js/app.js
+++ js/app.js
@@ -8,3 +8,59 @@
 // As a user. I want to see feedback if a letter(s) in the word I choose is in the right word and if it is there but its place is incorrect to show me feedback for that case
 // As a user. I want to see feedback if a letter(s) in the word I choose is in the right word and if it is there and its place is correct to show me feedback for that case
 // AS a user. I want to see feedback if I win/lose and how much tries I had left
+const gameBoard = [['', '', '', '', ''],
+['', '', '', '', ''],
+['', '', '', '', ''],
+['', '', '', '', ''],
+['', '', '', '', ''],
+['', '', '', '', '']]
+const wordList = [
+    "apple", "brave", "cabin", "delta", "eagle", "fancy", "grape", "hover", "input", "joker",
+    "knock", "latch", "mango", "noble", "ocean", "piano", "queen", "robin", "scale", "tiger",
+    "urban", "vivid", "waltz", "xenon", "yacht", "zebra", "angle", "beach", "candy", "drill",
+    "eager", "flood", "glide", "honey", "ideal", "jolly", "kneel", "lemon", "mirth", "nasty",
+    "orbit", "paint", "quiet", "raise", "shine", "table", "unite", "voter", "whale", "yield",
+    "zesty", "abide", "blaze", "chess", "ditch", "envoy", "feast", "giant", "haste", "image",
+    "jumpy", "karma", "lunar", "moist", "nerdy", "olive", "punch", "quilt", "rally", "spear",
+    "twist", "udder", "vigor", "worry", "xylem", "young", "zonal", "acorn", "badge", "couch",
+    "daisy", "elbow", "frail", "gleam", "haunt", "irony", "jelly", "knife", "latch", "mimic",
+    "niece", "oxide", "plaza", "quirk", "risky", "sloop", "tweak", "unzip", "vixen", "witty"
+]
+const randomWord = wordList[Math.floor(Math.random() * wordList.length)]
+let currentRow = 0
+let gamestate = false
+let currentCol = 0
+
+// cached event listeners
+const keys = document.querySelectorAll('.key-button')
+// functions
+const handleKeyPress = (keyValue) =>{
+    if(currentCol < 5){
+        if (keyValue == 'DELETE'){
+            console.log('Delete');
+            deleteLetter()
+        } else if (keyValue == 'ENTER'){
+            console.log('Submit');
+            submitGuess()
+        } else{
+            const tile = document.querySelector(`#tile-${currentRow}-${currentCol}`)
+            console.log(`#tile-${currentRow}-${currentCol}`)
+            tile.textContent = keyValue
+            currentCol++
+        }  
+    } 
+}
+const deleteLetter = () =>{
+
+}
+const submitGuess = () =>{
+    
+}
+// eventlisteners
+keys.forEach(key => {
+    key.addEventListener('click', () => {
+        const keyValue = key.innerText 
+        console.log(keyValue);
+        handleKeyPress(keyValue)
+    })
+});
