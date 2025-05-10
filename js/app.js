b344734d880f0834fc86c54460f702e803d881c6
leuim <jassim.mohammed48@gmail.com>
2025-05-10 14:16:07 +0300

    add the main layout of the page and some css stylings

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// As a user. I want to see a landing page when I arrive at the website to know I am in the right place
+// As a user. I want to see a tutorial on how to play the game. to know how to play the game
+// As a user. I want to see and click on keyboard layout in the page to type in words
+// As a user. I want to recieve feedback when I click on a letter on the keyboard. to know that my input has been saved
+// As a user. I want to be able to click back to remove letters from the word i want to write> so I can change it 
+// As a user. I want to be able to click submit when I am ready to submit my word. 
+// As a user. I want to see feedback based on my choice of word comparing to the right word
+// As a user. I want to see feedback if a letter(s) in the word I choose is in the right word and if it is there but its place is incorrect to show me feedback for that case
+// As a user. I want to see feedback if a letter(s) in the word I choose is in the right word and if it is there and its place is correct to show me feedback for that case
+// AS a user. I want to see feedback if I win/lose and how much tries I had left
